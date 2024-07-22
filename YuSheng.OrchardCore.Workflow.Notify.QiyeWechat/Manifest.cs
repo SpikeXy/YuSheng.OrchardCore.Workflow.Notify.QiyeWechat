625e0b55418e4097a06725a2ecf18b405a35cba2
spike <spike.li.han@gmail.com>
2024-07-23 06:30:20 +0800

    update name

--- YuSheng.OrchardCore.Workflow.Notify.QiyeWechat/Manifest.cs
+++ YuSheng.OrchardCore.Workflow.Notify.QiyeWechat/Manifest.cs
@@ -1,15 +1,15 @@
 using OrchardCore.Modules.Manifest;
 
 [assembly: Module(
-    Name = "YuSheng OrchardCore Workflow QiyeWechat Notify ",
+    Name = "YuSheng OrchardCore Workflow Notify QiyeWechat",
     Author = "spike",
     Website = "",
     Version = "0.0.1"
 )]
 
 [assembly: Feature(
-    Id = "YuSheng OrchardCore Workflow QiyeWechat Notify",
-    Name = "YuSheng OrchardCore Workflow QiyeWechat Notify",
+    Id = "YuSheng OrchardCore Workflow Notify QiyeWechat",
+    Name = "YuSheng OrchardCore Workflow Notify QiyeWechat",
     Description = "Provides qiyewechat notify ",
     Dependencies = new[] { "OrchardCore.Workflows" },
     Category = "Workflows"
